98723f76a2d5e692c69506f96adee144a3a16f3e
MG TOPHAZ <130512949+MGTOPHAZ@users.noreply.github.com>
2024-02-24 00:18:37 +0300

    Create MGTOPHAZ.py

---
+++ Gallery/audio/MGTOPHAZ.py
@@ -0,0 +1,24 @@
+// ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
+//▮ZENON BOT INC 2023 ®️ALL RIGHTS RESERVED
+//▮
+//▮FORK AND DON'T FORGET TO GIVE A STAR
+//▮
+//▮  Https://github.com/MGTOPHAZ/ZENON-AI 
+//▮
+//▮THIS SOFTWARE IS UNDER IS COPYRIGHT
+//▮
+//▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
+//▮
+//▮WHATSAPP US : +254705243111
+//▮EMAIL US Mgtophaz@gmail.com 
+//▮
+//╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+//
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━
+//┃THIS SOFTWARE INCLUDES 
+//┃SOME ENCRYPTED FILES
+//┃
+//┃THANKS FOR CHOOSING ZENON
+//┃I WROTE THIS SCRIPT FOR EVERYONE DONT SELL IT
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━
+//
