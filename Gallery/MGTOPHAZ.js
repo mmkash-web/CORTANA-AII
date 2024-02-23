70c16d04c56c0a7dd93588d8fdc255377904eefa
MG TOPHAZ <130512949+MGTOPHAZ@users.noreply.github.com>
2024-02-24 00:13:30 +0300

    Add files via upload

---
+++ Gallery/MGTOPHAZ.js
@@ -0,0 +1,59 @@
+
+apt update
+
+apt upgrade
+
+pkg update && pkg upgrade
+
+pkg install bash
+
+pkg install libwebp
+
+pkg install git -y
+
+pkg install nodejs -y 
+
+pkg install ffmpeg -y 
+
+pkg install wget
+
+pkg install yarn
+
+pkg install imagemagick -y
+
+git clone Https://github.com/MGTOPHAZ/ZENON-AI 
+
+cd ZENON-AI
+
+rm -rf session
+
+yarn install
+
+npm start
+
+https://github.com/MGTOPHAZ/ZENON-AI
+
+// ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
+//▮ZENON BOT INC 2023 ®️ALL RIGHTS RESERVED
+//▮
+//▮FORK AND DON'T FORGET TO GIVE A STAR
+//▮
+//▮  Https://github.com/MGTOPHAZ/ZENON-AI 
+//▮
+//▮THIS SOFTWARE IS UNDER IS COPYRIGHT
+//▮
+//▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
+//▮
+//▮WHATSAPP US : +254705243111
+//▮EMAIL US Mgtophaz@gmail.com 
+//▮
+//╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+//
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━
+//┃THIS SOFTWARE INCLUDES 
+//┃SOME ENCRYPTED FILES
+//┃
+//┃THANKS FOR CHOOSING ZENON
+//┃I WROTE THIS SCRIPT FOR EVERYONE DONT SELL IT
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━
+//
